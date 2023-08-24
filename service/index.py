b06db46cebc4d93586c7a797fa507c3304f185e5
ReisG <romanryabcev2006@gmail.com>
2023-08-24 12:43:52 +0300

    создал общий файл

---
+++ service/index.py
@@ -0,0 +1,40 @@
+import logging
+
+import aiogram
+from aiogram import Bot, Dispatcher, types
+
+from module.BotInitObject import dp
+from module.Database import myDatabase
+
+from module.user_must_register import user_must_register
+
+import logging
+
+import config
+
+# aiogram imports
+import aiogram
+from aiogram import Bot, Dispatcher, types
+
+# including module to work with Finite State Mashine in aiogram
+from aiogram.dispatcher import FSMContext
+from module.UserState import UserState
+
+from module.BotInitObject import dp
+from module.Database import myDatabase
+
+from module.user_must_register import isRegistered
+from module.db_query_maker import selectQuery, modifyQuery
+
+import json
+import html
+import urllib.parse
+
+@dp.message_handler(commands=["getplan"])
+async def getplan_command(message : types.Message):
+    await message.answer("Ваш план потребления/производства на завтра:")
+    with open("grap.jpeg", "rb") as ph:
+        await message.answer_photo(types.InputFile(ph))
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.insert(types.InlineKeyboardButton(text="отказаться на завтра"))
+    await message.answer("За завтра требуется выдать 5 кВт*ч энергии и потребить не более 10 кВт*ч", reply_markup=keyboard)
